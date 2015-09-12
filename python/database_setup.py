e513def93662bae8dea9de6114176f1c8c99fd23
Thomas <sketchbang@gmail.com>
2015-09-12 13:24:23 -0500

    We did it!  #HypeTrainTruckin

--- python/database_setup.py
+++ python/database_setup.py
@@ -23,21 +23,20 @@ class Criminal(Base):
     id = Column(Integer, primary_key=True)
     infraction = Column(String(90))
 
-
 class Citation(Base):
 
     __tablename__ = 'citation'
     id = Column(Integer, primary_key=True)
     citation_number = Column(Integer, unique=True)
-    citation_date = Column(DateTime(timezone=False))
+    citation_date = Column(String(20))
     first_name = Column(String(20), nullable=False)
     last_name = Column(String(30), nullable=False)
-    date_of_birth = Column(DateTime)
+    date_of_birth = Column(String(20))
     defendant_address = Column(String(90))
     defendant_city = Column(String(50))
     defendant_state = Column(String(2))
     drivers_license_number = Column(String(15))
-    court_date = Column(DateTime(timezone=False))
+    court_date = Column(String(20))
     court_location = Column(String(40))
     court_address = Column(String(90))
 
